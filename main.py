507deab8a2767a865de07e0ce154d959a9f7211f
garysingh08 <gjagpal8@yahoo.com>
2026-01-07 14:01:17 -0800

    Update data fetching and save to CSV
    Fetch data from a new URL and save it as a CSV file.

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+import pandas as pd
 import requests as r
+url = "https://tstranchers.pythonanywhere.com/getData"
 
-resp = r.get("https://www.accountplusfinance.com/projects/user-agents/headers.php", headers={"User-Agent": "Testing-Git_Hub"})
-
-if resp.status_code == 200:
-  print(resp.text)
-
-else:
-  print("error")
+resp = r.get(url, params={"stock": "META"})
+df=pd.read_json(resp.text)
+df.to_csv("data.csv", index=False)
